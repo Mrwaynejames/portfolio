eb44e264f6f20fc7d2804570615249ae1adafdf9
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-04 07:58:39 -0800

    linked outside accounts

--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import Ilogo from '../../assets/images/initalslogo.png'
 import nameLogo from '../../assets/images/name.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -24,6 +25,23 @@ const Sidebar = () => (
                <FontAwesomeIcon className ='icon' icon={faEnvelope} color="#4d4d43" /> 
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jack-mcwilliams1996/">
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/Mrwaynejames?tab=repositories">
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.instagram.com/jackmcwillliams/">
+                    <FontAwesomeIcon icon={faInstagram} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
