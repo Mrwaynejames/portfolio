61a876f0b30ea6b1e58467bad2380cfaf68782d8
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-04 19:23:48 -0800

    added emailjs

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,37 +1,70 @@
 import './index.css'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+
+    const form = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
+    
+        emailjs
+          .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
+          .then(
+            () => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+            },
+            () => {
+              alert('Failed to send the message, please try again')
+            }
+          )
+      }
     return (
         <div className='container contact-page'>
-            <div className='text-zone'>
-                <h1>
-                    Contact Me
-                </h1>
-                <p>I am Looking forward to hearing from you
-
-                </p>
-                <div className='contact-form'>
-                    <form>
-                        <ul>
-                            <li className="half">
-                                <input type="text" name="name" placeholder="Name" required />
-                            </li>
-                            <li className="half">
-                                <input type="email" name="email" placeholder="Email" required />
-                            </li>
-                            <li>
-                                <input placeholder="Subject" type="text" name="subject" required />
-                            </li>
-                            <li>
-                                <textarea name="message" placeholder="Message" required />
-                            </li>
-                            <li>
-                                <input type="submit" className='flat-button' value="SEND" />
-                            </li>
-                        </ul>
-                    </form>
-                </div>
-            </div>
+            <div className="text-zone">
+          <h1>
+           Contact Me
+          </h1>
+          <p>
+            Please dont hesitate to reach out. I look forward to hearing from you.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
         </div>
     )
 }
